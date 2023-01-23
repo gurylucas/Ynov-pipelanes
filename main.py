c839aeb50a3dcb04c1ddcb6336aff68255286646
Lucas GURY <contact.gury@gmail.com>
2023-01-23 10:32:05 +0100

    corec flake

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from bottle import route, run
 from misc import add
 
+
 @route("/hello/")
 @route("/hello")
 def index():
     return "<b>Hello World !</b>"
 
+
 @route('/add/<a>/<b>')
 @route('/add/<a>/<b>/')
 def route_add(a, b):
     return {'result': add(a, b)}
+
 
 run(host="localhost", port=8080, reloader=True)
