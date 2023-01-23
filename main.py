934eb017ab0b61bd628ec0d8b7f0a9fa76d52b05
Lucas GURY <contact.gury@gmail.com>
2023-01-23 10:37:16 +0100

    oui

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def index():
     return "<b>Hello World !</b>"
 
 
-@route('/add/<a>/<b>')
-@route('/add/<a>/<b>/')
+@route("/add/<a>/<b>")
+@route("/add/<a>/<b>/")
 def route_add(a, b):
-    return {'result': add(a, b)}
+    return {"result": add(a, b)}
 
 
 run(host="localhost", port=8080, reloader=True)
